8381ecd10486dcba35acf9b31f366e3bb9c32c9a
Jake Aufderheide <jakeaufderheide@gmail.com>
2020-04-07 19:49:01 -0700

    Finishes project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +27,41 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  var quizBrain = Quizbrain();
+
+  void checkAnswer(bool userPickedAnswer) {
+    setState(() {
+      if (quizBrain.isAnswerCorrect(userPickedAnswer)) {
+        addCorrectAnswer();
+      } else {
+        addIncorrectAnswer();
+      }
+      quizBrain.nextQuestion();
+      if (quizBrain.isGameOver()) {
+        Alert(
+                context: context,
+                title: "GAME OVER",
+                desc: "You've finished the quiz!")
+            .show();
+        quizBrain.startOver();
+        scoreKeeper.clear();
+      }
+    });
+  }
+
+  void addCorrectAnswer() {
+    scoreKeeper.add(
+      Icon(Icons.check, color: Colors.green),
+    );
+  }
+
+  void addIncorrectAnswer() {
+    scoreKeeper.add(
+      Icon(Icons.close, color: Colors.red),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +74,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +98,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                checkAnswer(true);
               },
             ),
           ),
@@ -79,12 +116,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                checkAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  const Question(
+    this.question,
+    this.isTrue,
+  );
+
+  final String question;
+  final bool isTrue;
+}
